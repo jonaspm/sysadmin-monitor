2483565c61062b463fa8c2b82aad604eb3e9731b
JJonas Perusquia <jonaspm_99@outlook.com>
2017-04-20 02:43:06 -0600

    Leer access token de Dropbox desde krystal.conf Subir respaldo de informacion a Dropbox Crear ~/backup y guardar los respaldos dentro de cada carpeta IP

--- krystal.sh
+++ krystal.sh
@@ -4,33 +4,43 @@
 #Comentario: Programa de monitoreo
 
 config="krystal.conf"
-list=$(tail -n+2 <(tac $config))
+list=$(tail -n+3 <(tac $config))
+correo=$(echo $(cat $config | tail -n2) | awk '{print $1}')
+access_token=$(cat $config | tail -n1)
 
-clear
 commands[0]='df --total | grep total | awk '"'"'{print $5/1}'"'"
-commands[1]='uptime | awk '"'"'{print $4}'"'"
-commands[2]='uptime | awk '"'"'{print ($5+$6+$7)/3/$(getconf _NPROCESSORS_ONLN)}'"'"
+commands[1]='users | tr '"' ' '\n' "'| sort -u | wc -l'
+commands[2]='uptime | awk '"'"'{print ($8+$9+$10)*100/3/$(getconf _NPROCESSORS_ONLN)}'"'"
 commands[3]='free --total | grep Total | awk '"'"'{print $3/$2*100}'"'"
 
+textos[0]="Porcentaje de disco: "
+textos[1]="Usuarios conectados: "
+textos[2]="Porcentaje de carga del sistema: "
+textos[3]="Porcentaje de uso de RAM: "
+
+clear
 
 echo "n" | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ""
 
 while IFS=, read -r ip usuario contrasena; do
+	mkdir ~/backup/$ip 2> /dev/null
 	conexion="$usuario@$ip"
 	ssh-keygen -R $ip > /dev/null
 	ssh-keyscan $ip >> ~/.ssh/known_hosts
-	./contrasena $conexion $contrasena 2> /dev/null
-	mv ./backup ./backup.old 2> /dev/null
+	./exp_copy_id $conexion $contrasena 2> /dev/null
+	mv ~/backup/$ip/$usuario ~/backup/$ip/$usuario.old 2> /dev/null
 	for index in ${!commands[*]}; do
-		ssh -n -T $conexion ${commands[$index]} >> ./backup
+		resultado=$(ssh -n -T $conexion ${commands[$index]})
+		mensaje=$(echo ${textos[$index]} $resultado)
+		if [[ $(bc <<< "$resultado > 70") > 0 ]] && [[ $index -ne 1 ]]; then
+			printf $mensaje | mailx -v -s "Exceso de uso de recursos" $correo
+		fi
+		echo $mensaje >> ~/backup/$ip/$usuario
 	done
-	#DROPBOX
-	rm ./backup.old
+	curl -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer $access_token" --header "Dropbox-API-Arg: {\"path\": \"/backup/$ip/$usuario.txt\",\"mode\":\"add\",\"autorename\": true,\"mute\": false}" --header "Content-Type: application/octet-stream" --data-binary "@$(echo ~/backup/$ip/$usuario)"
+	rm ~/backup/$ip/$usuario.old
 done << EOF
 $list
 EOF
-correo=$(cat $config | tail -n1)
-
-#echo "MENSAJE" | mailx -v -s "Asunto" $correo
 
 exit 0
