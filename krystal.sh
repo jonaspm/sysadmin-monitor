e90d691b3f22ed19e0f0b1130019969d9eb6c854
JJonas Perusquia <jonaspm_99@outlook.com>
2017-04-21 17:15:00 -0600

    Especificar la ruta completa de instalación/ejecución. Asegurarse de que el instalador se ejecuta como root. Renombrar backup > sysadmin-monitor. Instalador ahora crea una tarea CRON y la ejecuta cada minuto. La ruta de instalación ahora es: /etc/cron.sysadmin-monitor

--- krystal.sh
+++ krystal.sh
@@ -3,10 +3,14 @@
 #Fecha: Tue Apr 11 12:30:19 MDT 2017
 #Comentario: Programa de monitoreo
 
+clear
+
+app_name="sysadmin-monitor"
+folder="/etc/cron.$app_name"
 config="krystal.conf"
-list=$(tail -n+3 <(tac $config))
-correo=$(echo $(cat $config | tail -n2) | awk '{print $1}')
-access_token=$(cat $config | tail -n1)
+list=$(tail -n+3 <(tac $folder/$config))
+correo=$(echo $(cat $folder/$config | tail -n2) | awk '{print $1}')
+access_token=$(cat $folder/$config | tail -n1)
 
 commands[0]='df --total | grep total | awk '"'"'{print $5/1}'"'"
 commands[1]='users | tr '"' ' '\n' "'| sort -u | wc -l'
@@ -18,29 +22,26 @@ textos[1]="Usuarios conectados: "
 textos[2]="Porcentaje de carga del sistema: "
 textos[3]="Porcentaje de uso de RAM: "
 
-clear
-
 echo "n" | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ""
 
 while IFS=, read -r ip usuario contrasena; do
-	mkdir ~/backup/$ip 2> /dev/null
+	mkdir ~/$app_name/$ip 2> /dev/null
 	conexion="$usuario@$ip"
 	ssh-keygen -R $ip > /dev/null
 	ssh-keyscan $ip >> ~/.ssh/known_hosts
-	./exp_copy_id $conexion $contrasena 2> /dev/null
-	mv ~/backup/$ip/$usuario ~/backup/$ip/$usuario.old 2> /dev/null
+	$folder/exp_copy_id $conexion $contrasena 2> /dev/null
+	mv ~/$app_name/$ip/$usuario ~/$app_name/$ip/$usuario.old 2> /dev/null
 	for index in ${!commands[*]}; do
 		resultado=$(ssh -n -T $conexion ${commands[$index]})
 		mensaje=$(echo ${textos[$index]} $resultado)
 		if [[ $(bc <<< "$resultado > 70") > 0 ]] && [[ $index -ne 1 ]]; then
 			printf $mensaje | mailx -v -s "Exceso de uso de recursos" $correo
 		fi
-		echo $mensaje >> ~/backup/$ip/$usuario
+		echo $mensaje >> ~/$app_name/$ip/$usuario
 	done
-	curl -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer $access_token" --header "Dropbox-API-Arg: {\"path\": \"/backup/$ip/$usuario.txt\",\"mode\":\"add\",\"autorename\": true,\"mute\": false}" --header "Content-Type: application/octet-stream" --data-binary "@$(echo ~/backup/$ip/$usuario)"
-	rm ~/backup/$ip/$usuario.old
+	curl -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer $access_token" --header "Dropbox-API-Arg: {\"path\": \"/$ip/$usuario.txt\",\"mode\":\"add\",\"autorename\": true,\"mute\": false}" --header "Content-Type: application/octet-stream" --data-binary "@$(printf ~/$app_name/$ip/$usuario)"
+	rm ~/$app_name/$ip/$usuario.old
 done << EOF
 $list
 EOF
-
 exit 0
